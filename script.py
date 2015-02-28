986a385781196e6152dd1e5999bb9fdc15a1741d
Yalk <yalk@users.noreply.github.com>
2015-02-28 20:31:48 +0530

    Create script.py
    feed csv data into a mysql database directly
    - added comments
    - some instructions

---
+++ script.py
@@ -0,0 +1,27 @@
+#refer sample csv file in /
+#remember to install the import modules used
+#replace stuff in capitals by appropriate values
+
+import csv
+import pymysql
+
+#mysql db connection
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='ENTER DATABASE NAME HERE')
+cur = conn.cursor()
+
+with open('ENTER FILENAME HERE.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+	i=0
+	for row in reader:
+		query = "insert into TABLENAME (column1, column2, column3) VALUES ('"+row['heading1']+"', '"+row['heading2']+"', '"+row['heading3']+"')"
+		cur.execute(query)
+		i+=1
+		if(i%1000==0):
+		  #commit after every 1000 queries executed
+			conn.commit()
+			print(i)
+		#final commit
+	conn.commit()
+cur.close()
+conn.close()
+print("Done")
